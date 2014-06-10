97d3b7d8ca0d13327ee165a0b16f6f62b2992c48
Yuyu <yuwendai.d@gmail.com>
2014-06-10 14:06:21 -0700

    Method 1: Calculate inversions
    Loop

--- Course/homework1/inversions.c
+++ Course/homework1/inversions.c
@@ -1,41 +1,45 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#define LEN 6
+#define LEN 100000
 int main(int argc, char const *argv[])
 {
-	/* Read from File */
-	FILE *fp;
-	char *line;
-	size_t len;
-	ssize_t read;
-	int array[LEN];
-	fp = fopen("test.txt", "r");
-	if ( fp == NULL )
-		exit(1);
-	int i = 0;
-	while ((read = getline(&line, &len, fp)) != -1) 
-	{
-		//printf("Retrieved line of length %zu :\n", read);
+    /* Read from File */
+    FILE *fp = NULL; // Pointer must be initialized
+    char *line = NULL;
+    size_t len;
+    ssize_t read;
+    int array[LEN];
+    fp = fopen("IntegerArray.txt", "r");
+    if ( fp == NULL )
+        exit(1);
+    int i = 0;
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        //printf("Retrieved line of length %zu :\n", read);
         //printf("%s", line);
         sscanf(line, "%d", &array[i]);
         i++;
+        printf("Have read %d lines!\n", i);
     }
-    for (i = 0;i < LEN;i++){
-    	printf("%d\n", array[i] );
+
+    /*
+
+    for (i = 0; i < LEN; i++) {
+        printf("%d\n", array[i] );
     }
-    int count = 0;
-    int m=0;
-    int n = 0;
+
+    */
+    long count = 0; // int is smaller for the estimated result: 5,0000,00000, which can be checked in limits.h
+    int m, n;
     for( m = 0; m < LEN-1; m++)
     {
-    	printf("%d\n", m );
-    	for(n = 0; n < LEN; n++)
-    	{
-    		if (array[m] > array[n])
-    			count = count + 1;
-    		printf("%d\n", n);
-    	}
+        for(n = m+1; n < LEN; n++)
+        {
+            if (array[m] > array[n])
+                count = count + 1;
+        }
     }
-    printf("%d\n", count);
-	return 0;
+    printf("There are %lu inversions in this array!\n", count);
+
+    return 0;
 }
